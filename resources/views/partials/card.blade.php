6b7f88852ec302b3b611bb1413db8bc885a87801
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-10 16:18:28 +0200

    fix: style in card.blade.php

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,10 +1,10 @@
 <div class="card mb-4">
     <img src="{{ $movie->image }}" alt="movie_image">
     <div class="card-body">
-        <p>{{ $movie->title }}</p>
-        <p>{{ $movie->original_title }}</p>
-        <p>{{ $movie->nationality }}</p>
-        <p>{{ $movie->date }}</p>
-        <p>{{ $movie->vote }}</p>
+        <p><span class="fw-bold text-success">Titolo:</span> {{ $movie->title }}</p>
+        <p><span class="fw-bold text-success">Titolo Originale:</span> {{ $movie->original_title }}</p>
+        <p><span class="fw-bold text-success">Nazionalità:</span> {{ $movie->nationality }}</p>
+        <p><span class="fw-bold text-success">Data uscita:</span> {{ $movie->date }}</p>
+        <p><span class="fw-bold text-success">Voto:</span> {{ $movie->vote }}</p>
     </div>
 </div>
