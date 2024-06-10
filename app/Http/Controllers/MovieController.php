6803eac4c812b632dc54aa238bacecf5162db045
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-10 15:07:46 +0200

    add: route in web.php

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index() {
+        $movies = Movie::all();
+        dd($movies);
+        return view('books');
+    }
 }
